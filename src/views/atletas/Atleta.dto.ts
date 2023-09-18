a0a68e99a1af9a180fda469d57746c07489e9134
samurai01 kgsh <samurai01.kgsh@gmail.com>
2023-09-17 21:35:52 -0300

    chore: criar dto de atleta

---
+++ src/views/atletas/Atleta.dto.ts
@@ -0,0 +1,15 @@
+export interface AtletaDto {
+    codigo: string
+    nome: string
+    dataNascimento: string
+    nacionalidade: string
+    ufNascimento: string
+    cidadeNascimento: string
+    sexo: string
+    rg: string
+    cpf: string
+    email: string
+    telCelular: string
+    religiao: string
+    id: string
+}
