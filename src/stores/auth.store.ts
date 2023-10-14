29c9e18813b15492e17d08c1d36867bb6e4c96a6
samurai01 kgsh <samurai01.kgsh@gmail.com>
2023-10-14 12:26:48 -0300

    ci: usar application settings do azure

--- src/stores/auth.store.ts
+++ src/stores/auth.store.ts
@@ -4,7 +4,7 @@ import { fetchWrapper } from '@/helpers';
 import { router } from '@/router';
 import { useAlertStore } from '@/stores';
 
-const baseUrl = `${import.meta.env.API_URL}/token`;
+const baseUrl = `${import.meta.env.VITE_API_URL}/token`;
 
 export const useAuthStore = defineStore({
     id: 'auth',
