d06e322c0fab4efe982c9aac205cd9b79dacc949
samurai01 kgsh <samurai01.kgsh@gmail.com>
2023-10-18 17:36:15 -0300

    feat: criar componente radio para campo genero

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Alert } from '@/components/Alert.vue';
 export { default as Nav } from '@/components/Nav.vue';
+export { default as  TheGeneroRadio } from '@/components/TheGeneroRadio.vue';
